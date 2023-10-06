196672df490a154493c5f66d67ae1907a03c866a
Nagesh Pandey <125539116+nagesh44890@users.noreply.github.com>
2023-10-06 19:10:22 +0530

    commiting

--- 02controlstatement_cpp/01.cpp
+++ 02controlstatement_cpp/01.cpp
@@ -16,5 +16,20 @@ int main(){
 else{
     cout<<"your number is odd"<<endl;
 }
+
+// if ...else ladder
+int score;
+cout << "enter your score between 0-100\n";
+cin >> score;
+
+if(score >= 90){
+    cout<< "your grade : A\n";
+}else if(score >= 70 && score < 90){
+    cout << "your grade : B\n";
+}else if(score >= 50 && score < 70){
+    cout<< "your grade : c\n";
+}else{
+    cout << "your grade : failed\n";
+}
 return 0;
 }
