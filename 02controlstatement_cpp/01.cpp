016a59fd4fa6c75fc9c46e953502dd44b1aca1a5
Nagesh Pandey <125539116+nagesh44890@users.noreply.github.com>
2023-10-06 18:50:08 +0530

    commit

---
+++ 02controlstatement_cpp/01.cpp
@@ -0,0 +1,20 @@
+
+
+#include <iostream>
+using namespace std;
+int main(){
+
+
+ // checking even and odd number
+    int n;
+    cout << "enter a number: "<<endl;
+    cin >>n;
+    if( n % 2 == 0){
+        cout<<"your number is even"<<endl;
+
+    }
+else{
+    cout<<"your number is odd"<<endl;
+}
+return 0;
+}
