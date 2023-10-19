5b59060cd4b563fd0b92b37c05fe78b574ca9a18
Nagesh Pandey <125539116+nagesh44890@users.noreply.github.com>
2023-10-19 19:34:26 +0530

    committed

--- 03function_cpp/02function.cpp
+++ 03function_cpp/02function.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include<bits/stdtr1c++.h>
 using namespace std;
 
 //function to convert character from lowercase to uppercase
@@ -10,13 +10,45 @@ char convert(char name)
 
 //function to check armstrong number
 
+int countdigit(int n)  //function to count digit in a number
+{
+  int count = 0;
+  while (n)
+  {
+    count++;
+    n/=10;
+  }
+  return count; 
+}
+
+bool armstrong(int num, int digit)
+{
+    int n = num, ans = 0, rem;
+    while(n)
+    {
+        rem = n%10;
+        n/=10;
+        ans = ans + pow(rem, digit);
+
+    }
+    if(ans==num)
+    return 1;
+    else
+    return 0;
+
+}
 
 int main(){
-    char name;
-    cin>>name;
+    // char name;
+    // cin>>name;
+    // cout<<convert(name); //function call for character conversion
 
-    cout<<convert(name); //function call for character conversion
-
-
+    int num;
+    cin >> num;
+    
+    //count digit
+    int digit = countdigit(num);
+    //armstrong number
+    cout << armstrong(num, digit);
     return 0;
 }
