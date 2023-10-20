143b36af43040eb437bdc6b22187e25a753c9450
Nagesh Pandey <i.am.nageshpandey@gmail.com>
2023-10-20 16:18:59 +0530

    commit

--- 02controlstatement_cpp/01.cpp
+++ 02controlstatement_cpp/01.cpp
@@ -4,32 +4,17 @@
 using namespace std;
 int main(){
 
+int number;
+cout<<"enter a number: ";
+cin >> number;
 
- // checking even and odd number
-    int n;
-    cout << "enter a number: "<<endl;
-    cin >>n;
-    if( n % 2 == 0){
-        cout<<"your number is even"<<endl;
-
-    }
+if(number%2 == 0)
+{
+    cout<<"even";
+}
 else{
-    cout<<"your number is odd"<<endl;
+    cout<<"odd";
 }
 
-// if ...else ladder
-int score;
-cout << "enter your score between 0-100\n";
-cin >> score;
-
-if(score >= 90){
-    cout<< "your grade : A\n";
-}else if(score >= 70 && score < 90){
-    cout << "your grade : B\n";
-}else if(score >= 50 && score < 70){
-    cout<< "your grade : c\n";
-}else{
-    cout << "your grade : failed\n";
-}
 return 0;
 }
