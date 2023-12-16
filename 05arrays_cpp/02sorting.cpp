465e0d16a4c4bce15d0f2c1c972017708de94d77
Nagesh Pandey <i.am.nageshpandey@gmail.com>
2023-12-16 14:42:57 +0530

    commit

--- 05arrays_cpp/02sorting.cpp
+++ 05arrays_cpp/02sorting.cpp
@@ -5,7 +5,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-// selection sort.... uses in-place comparison to rearrange elements in several rounds or iterations.
+
 
 int arr[1000];
 int n;
@@ -17,22 +17,36 @@ for (int i = 0; i <n; i++)
     cin>>arr[i];
 }
 
-for(int i = 0; i<n-1; i++){
-   int index = i;
-   for(int j = i+1; j<n; j++){
-    if(arr[j] < arr[index])
-    index = j;
-   }
-    swap(arr[index], arr[i]);
-}
+//selection sort logic
+// selection sort.... uses in-place comparison to rearrange elements in several rounds or iterations.
 
+// for(int i = 0; i<n-1; i++){
+//    int index = i;
+//    for(int j = i+1; j<n; j++){
+//     if(arr[j] < arr[index])
+//     index = j;
+//    }
+//     swap(arr[index], arr[i]);
+// }
+
+
+//bubble sort logic
+//bubble sort....works by repeatedly swapping the adjacent elements if they are in wrong order
+
+// for(int i = n-2; i>=0; i--){
+//     for(int j = 0; j<=i; j++){
+//         if(arr[j] > arr[j+1]){
+//             swap(arr[j], arr[j+1]);
+//         }
+//     }
+// }
+
+
+//insertion sort logic
+//
 cout<<"the sorted element of array in ascending oreder: ";
 for(int i = 0; i<n; i++){
     cout<< arr[i] <<" ";
 }
-
-//
-
-
 return 0;
 }
