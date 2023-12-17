f47f3f64cdffe4a0328c71d16c7e64b04ffb9920
Nagesh Pandey <i.am.nageshpandey@gmail.com>
2023-12-17 16:11:38 +0530

    commit

--- 05arrays_cpp/02sorting.cpp
+++ 05arrays_cpp/02sorting.cpp
@@ -4,8 +4,63 @@
 
 #include <bits/stdc++.h>
 using namespace std;
-int main(){
 
+
+//merge sort logic
+//merge sort....works by dividing an array into smaller subarray,sorting each subarray,and then merging the sorted subarray
+
+void merge(int arr[], int start, int mid, int end){
+
+    vector<int>temp(end-start+1);
+    int left = start, right = mid + 1, index = 0;
+
+    while(left<=mid && right<=end){
+        if(arr[left]<=arr[right]){
+            temp[index] = arr[left];
+            index++, left++;
+        }
+        else{
+            temp[index] = arr[right];
+            index++, right++;
+        }
+    }
+    //left array is not empty yet
+    while(left<=mid){
+        temp[index]=arr[left];
+        index++, left++;
+    }
+    //right array is not empty yet
+    while(right<=end){
+        temp[index]=arr[right];
+        index++, right++;
+    }
+
+    index = 0;
+    //put these value in array
+    while(start<=end){
+        arr[start]=temp[index];
+        start++, index++;
+    }
+
+}
+
+void mergesort(int arr[], int start, int end){
+
+    int mid = start + (end - start )/2;
+    
+    //left side
+    mergesort(arr, start, mid);
+
+    //right side
+    mergesort(arr, mid + 1, end);
+
+    merge(arr, start, mid, end);
+
+}
+
+
+
+int main(){
 
 int arr[1000];
 int n;
@@ -45,17 +100,22 @@ for (int i = 0; i <n; i++)
 //insertion sort logic
 //insertion sort.....works by dividing array into two parts,sorted and unsorted
 
-for(int i = 1; i<n; i++){
-    for(int j = i; j>0; j--){
-        if(arr[j] < arr[j-1])
-        swap(arr[j], arr[j-1]);
-        else
-        break;
-    }
-}
+// for(int i = 1; i<n; i++){
+//     for(int j = i; j>0; j--){
+//         if(arr[j] < arr[j-1])
+//         swap(arr[j], arr[j-1]);
+//         else
+//         break;
+//     }
+// }
+
+
+
+//calling mergesort function
+mergesort(arr, 0, n-1);
 
 //printing sorted array elements
-cout<<"the sorted element of array in ascending oreder: ";
+cout<<"the sorted element of array in ascending order: ";
 for(int i = 0; i<n; i++){
     cout<< arr[i] <<" ";
 }
