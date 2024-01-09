e4a891cc67a3df1d0f3016c8b6d79d6d31497a98
Nagesh Pandey <i.am.nageshpandey@gmail.com>
2024-01-09 21:11:46 +0530

    commit

--- 08File_handling/01.cpp
+++ 08File_handling/01.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
   ofstream myfile;
   myfile.open("example.txt");
-  myfile << "Writing this to a file.\n";
+  myfile << "Writing this to a file first time.\n";
   myfile.close();
   return 0;
 }
