40d919e21a5afa2942067566c24027145524d81f
Nagesh Pandey <i.am.nageshpandey@gmail.com>
2024-02-16 19:13:34 +0530

    commit

--- Queue/02.cpp
+++ Queue/02.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<queue>
+#include<stack>
 using namespace std;
 
 int main(){
@@ -9,6 +10,41 @@ int main(){
     q.push(10);
     q.push(20);
     q.push(30);
+    q.push(40);
+    q.push(50);
+    q.push(60);
+    q.push(70);
+    q.push(80);
+    q.push(90);
+    q.push(100);
+
+    // //print all element of queue
+    int n = q.size();
+    // while(n--){
+    //     cout<<q.front()<<" ";
+    //     q.push(q.front());
+    //     q.pop();
+    // }
+    // cout<<endl;
+
+    //queue reversal
+    stack<int> st;
+    while(!q.empty()){
+        st.push(q.front());
+        q.pop();
+    }
+    while(!st.empty()){
+        q.push(st.top());
+        st.pop();
+    }
+    
+    //print element of queue
+    while(n--){
+        cout<<q.front()<<" ";
+        q.push(q.front());
+        q.pop();
+    }
+    cout<<endl;
 
     //delete
     q.pop();
